fd9613baaf2e7db5d156844677358ee5f1751121
P-Rajyasri2005 <122425193+P-Rajyasri2005@users.noreply.github.com>
2023-11-30 10:21:42 +0530

    Add files via upload

---
+++ Skeleton code for online food processing system.py
@@ -0,0 +1,60 @@
+# coding=System
+from CUSTOMER import *
+from RESTURANT import *
+from BANK import *
+from SODEX_SYASTEM import *
+from PAYMENT import *
+
+class SYSTEM_ORDER(object):
+
+  """
+   
+
+  :version:
+  :author:
+  """
+
+  """ ATTRIBUTES
+
+   
+
+  menu_cards  (private)
+
+   
+
+  resturant  (private)
+
+   
+
+  customer_id  (private)
+
+   
+
+  customer_password  (private)
+
+   
+
+  payment  (private)
+
+  """
+
+  def verify(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+  def update(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+
+
